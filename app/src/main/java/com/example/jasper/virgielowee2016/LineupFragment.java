864a6c840e9095e77e5be8d4e5c1d4fb0fee877c
Jasper van Tilburg <J.F.vantilburg@student.tudelft.nl>
2016-06-22 23:22:13 +0200

    Lineup tabs added

--- app/src/main/java/com/example/jasper/virgielowee2016/LineupFragment.java
+++ app/src/main/java/com/example/jasper/virgielowee2016/LineupFragment.java
@@ -2,7 +2,12 @@ package com.example.jasper.virgielowee2016;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +23,88 @@ public class LineupFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static int int_items = 4;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lineup, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /**
+         *Inflate tab_layout and setup Views.
+         */
+        View x =  inflater.inflate(R.layout.fragment_lineup,null);
+        tabLayout = (TabLayout) x.findViewById(R.id.tab_layout);
+        viewPager = (ViewPager) x.findViewById(R.id.viewpager);
+
+        /**
+         *Set an Apater for the View Pager
+         */
+        viewPager.setAdapter(new MyAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return x;
+
     }
 
+    class MyAdapter extends FragmentPagerAdapter{
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 : return new MondayFragment();
+                case 1 : return new TuesdayFragment();
+                case 2 : return new WednesdayFragment();
+                case 3 : return new ThursdayFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return int_items;
+
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return "Ma";
+                case 1 :
+                    return "Di";
+                case 2 :
+                    return "Wo";
+                case 3 :
+                    return "Do";
+            }
+            return null;
+        }
+    }
 }
